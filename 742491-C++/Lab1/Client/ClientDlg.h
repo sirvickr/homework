474601dfa09dcb6d60caf3c731906b0c4c86d1d8
sirvickr1 <sirvickr@gmail.com>
2017-11-20 20:27:57 +0300

    [+ Заготовка для Lab1/Client

---
+++ 742491-C++/Lab1/Client/ClientDlg.h
@@ -0,0 +1,31 @@
+
+// ClientDlg.h : header file
+//
+
+#pragma once
+
+
+// CClientDlg dialog
+class CClientDlg : public CDialogEx
+{
+// Construction
+public:
+	CClientDlg(CWnd* pParent = NULL);	// standard constructor
+
+// Dialog Data
+	enum { IDD = IDD_CLIENT_DIALOG };
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV support
+
+
+// Implementation
+protected:
+	HICON m_hIcon;
+
+	// Generated message map functions
+	virtual BOOL OnInitDialog();
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+};
