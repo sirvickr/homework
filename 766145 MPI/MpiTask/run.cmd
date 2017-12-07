abc58190f0c66f001c5f1ef4b915f8b691ee9779
sirvickr <sirvickr@gmail.com>
2017-12-07 17:58:47 +0300

    + run.cmd

---
+++ 766145 MPI/MpiTask/run.cmd
@@ -0,0 +1,5 @@
+"c:\Program Files\Microsoft MPI\Bin\mpiexec.exe" -n 4 x64\Release\MpiTask.exe
+
+rem "C:\Program Files\Microsoft HPC Pack 2008 SDK\Bin\mpiexec.exe" -n 3 x64\Release\MpiTask.exe
+
+PAUSE >nul
