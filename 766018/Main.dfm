977f1c06559a6151804117c258e5bb8377494b05
sirvickr <sirvickr@gmail.com>
2017-12-12 17:22:24 +0300

    [+] 766018 (C++Builder: визуализация оптической задачки)

---
+++ 766018/Main.dfm
@@ -0,0 +1,16 @@
+object FMain: TFMain
+  Left = 472
+  Top = 210
+  Width = 1201
+  Height = 702
+  Caption = 'FMain'
+  Color = clBtnFace
+  Font.Charset = DEFAULT_CHARSET
+  Font.Color = clWindowText
+  Font.Height = -11
+  Font.Name = 'MS Sans Serif'
+  Font.Style = []
+  OldCreateOrder = False
+  PixelsPerInch = 96
+  TextHeight = 13
+end
---
+++ 766018/Main.h
@@ -0,0 +1,21 @@
+//---------------------------------------------------------------------------
+
+#ifndef MainH
+#define MainH
+//---------------------------------------------------------------------------
+#include <Classes.hpp>
+#include <Controls.hpp>
+#include <StdCtrls.hpp>
+#include <Forms.hpp>
+//---------------------------------------------------------------------------
+class TFMain : public TForm
+{
+__published:	// IDE-managed Components
+private:	// User declarations
+public:		// User declarations
+	__fastcall TFMain(TComponent* Owner);
+};
+//---------------------------------------------------------------------------
+extern PACKAGE TFMain *FMain;
+//---------------------------------------------------------------------------
+#endif
