6a6749521dd975a9d20b64d325bc4fd5c263677e
sirvickr <sirvickr@gmail.com>
2018-05-16 23:00:29 +0300

    * 859014 (C sorting): hsort заработал

--- 859014-c-sorting/hsort/hsort.c
+++ 859014-c-sorting/hsort/hsort.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
 #include <string.h>
-#include <limits.h>
 #include <stdlib.h>
 
-// вывести массив на экран (с подписью и в нужном формате)
+#define ITEM_TYPE char*
+#define HEAP_PARENT( i )	( ( ( ( i ) + 1 ) >> 1 ) - 1 )
+#define HEAP_LEFT( i )		( ( ( ( ( i ) + 1 ) << 1 ) + 0 ) - 1 )
+#define HEAP_RIGHT( i )		( ( ( ( ( i ) + 1 ) << 1 ) + 1 ) - 1 )
+
 static void print_array(char **a, const size_t len, const char *legend) {
 	size_t i;
 	printf(legend);
@@ -26,75 +29,46 @@ static int compare_a_count(const void* a, const void* b) {
 	return calc_a_count((const char*)a) - calc_a_count((const char*)b);
 }
 
-static void swap(char** a, char** b) {
+static void str_swap(char** a, char** b) {
 	char* s = *a;
 	*a = *b;
 	*b = s;
 }
 
-#if 0
-static void fix_down(char** a, size_t k, size_t n) {
-	size_t i;
-	while(k * 2 <= n) {
-		i = 2 * k;
-		if(i < n && calc_a_count(a[i]) < calc_a_count(a[i + 1]))
-			i++;
-		if(!(calc_a_count(a[k]) < calc_a_count(a[i])))
-			break;
-		swap(&a[k], &a[i]);
-		k = i;
-	}
-}
-#else
-void fix_down(char** a, int root, int bottom)
+static void fix_down( ITEM_TYPE* a, int n, int i )
 {
-  int maxChild; // индекс максимального потомка
-  int done = 0; // флаг того, что куча сформирована
-  // Пока не дошли до последнего ряда
-  while ((root * 2 <= bottom) && (!done)) 
-  {
-    if (root * 2 == bottom)    // если мы в последнем ряду,
-      maxChild = root * 2;    // запоминаем левый потомок
-    // иначе запоминаем больший потомок из двух
-    else if (calc_a_count(a[root * 2]) > calc_a_count(a[root * 2 + 1]))
-      maxChild = root * 2;
-    else
-      maxChild = root * 2 + 1;
-    // если элемент вершины меньше максимального потомка
-    if (calc_a_count(a[root]) < calc_a_count(a[maxChild])) 
-    {
-		swap(&a[root], &a[maxChild]);
-		root = maxChild;
-    }
-    else // иначе
-      done = 1; // пирамида сформирована
-  }
-}
-#endif
-//static void hsort(void* base, size_t nel, size_t width, int (*compare)(const void* a, const void* b)) {
-#if 0
-static void hsort(char** base, size_t nel, size_t width, int (*compare)(const void* a, const void* b)) {
-	int i, n = nel;
-	for(i = n / 2; i >= 1; i--) {
-		fix_down(base, i, n);
-	}
-	while(n > 1) {
-		swap(&base[1], &base[n]);
-		fix_down(base, 1, --n);
+	int l, r, largest;
+	l = HEAP_LEFT( i );
+	r = HEAP_RIGHT( i );
+	if ( l < n && compare_a_count( a[ l ], a[ i ] ) > 0 )
+		largest = l;
+	else
+		largest = i;
+	if ( r < n && compare_a_count( a[ r ], a[ largest ] ) > 0 )
+		largest = r;
+	if ( largest != i ) {
+		str_swap( &a[ largest ], &a[ i ] );
+		fix_down( a, n, largest );
 	}
 }
-#else
-static void hsort(char** base, size_t nel, size_t width, int (*compare)(const void* a, const void* b)) {
-	int i;
-	for(i = nel / 2 - 1; i >= 0; i--) {
-		fix_down(base, i, nel);
-	}
-	for(i = nel - 1; i >= 1; i--) {
-		swap(&base[0], &base[i]);
-		fix_down(base, 0, i - 1);
+
+static void build_heap( ITEM_TYPE* a, int n )
+{
+	for ( int i = ( n >> 1 ) - 1; i >= 0; i-- ) {
+		fix_down( a, n, i );
 	}
 }
-#endif
+
+//static void hsort(void* base, int nel, int width, int (*compare)(const void* a, const void* b))
+static void hsort(ITEM_TYPE* base, int nel, int width, int (*compare)(const void* a, const void* b))
+{
+	build_heap( base, nel );
+	for ( int i = nel - 1; i > 0; i-- ) {
+		str_swap( &base[ 0 ], &base[ i ] );
+		nel--;
+		fix_down( base, nel, 0 );
+	}
+}
 
 int main(int argc, char* argv[]) {
 	const size_t MAX_BUF_LEN = 512;
