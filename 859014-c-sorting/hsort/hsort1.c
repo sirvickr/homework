071e5ccb5e5c49a0046efff73d143be0c8cad0c1
Виктор Краснов <vickr@stc-spb.ru>
2018-05-17 13:06:06 +0300

    * 859014 (C sorting): hsort1: черновик

--- 859014-c-sorting/hsort/hsort1.c
+++ 859014-c-sorting/hsort/hsort1.c
@@ -1,80 +1,137 @@
 #include <stdio.h>
 #include <string.h>
-#include <limits.h>
 #include <stdlib.h>
 
-// вывести массив на экран (с подписью и в нужном формате)
-static void print_array(const int *a, const size_t len, const char *legend) {
+#define ITEM_TYPE char*
+#define HEAP_PARENT( i )	( ( ( ( i ) + 1 ) >> 1 ) - 1 )
+#define HEAP_LEFT( i )		( ( ( ( ( i ) + 1 ) << 1 ) + 0 ) - 1 )
+#define HEAP_RIGHT( i )		( ( ( ( ( i ) + 1 ) << 1 ) + 1 ) - 1 )
+
+typedef int (*compare_t)(const void* a, const void* b);
+
+static char swap_buff[sizeof(ITEM_TYPE)];
+
+static void print_array(char **a, const size_t len, const char *legend) {
 	size_t i;
-	printf(legend);
+	printf("%s", legend);
 	for (i = 0; i < len; i++)
-		printf("%d ", a[i]);
+		printf("%s ", a[i]);
 	printf("\n");
 }
 
-static void swap(int* a, int* b) {
-	int t = *a;
-	*a = *b;
-	*b = t;
+static int calc_a_count(const char* s) {
+	int i, count = 0, slen = strlen(s);
+	for(i = 0; i < slen; i++) {
+		if(s[i] == 'a') {
+			count++;
+		}
+	}
+	return count;
 }
 
-void fixDown(int *a, int root, int bottom)
-{
-  int maxChild; // индекс максимального потомка
-  int done = 0; // флаг того, что куча сформирована
-  // Пока не дошли до последнего ряда
-  while ((root * 2 <= bottom) && (!done)) 
-  {
-    if (root * 2 == bottom)    // если мы в последнем ряду,
-      maxChild = root * 2;    // запоминаем левый потомок
-    // иначе запоминаем больший потомок из двух
-    else if (a[root * 2] > a[root * 2 + 1])
-      maxChild = root * 2;
-    else
-      maxChild = root * 2 + 1;
-    // если элемент вершины меньше максимального потомка
-    if (a[root] < a[maxChild]) 
-    {
-		swap(&a[root], &a[maxChild]);
-		root = maxChild;
-    }
-    else // иначе
-      done = 1; // пирамида сформирована
-  }
+static int compare_a_count(const void* a, const void* b) {
+	return calc_a_count(*(const char**)a) - calc_a_count(*(const char**)b);
 }
 
-void heapSort(int *a, int nel) 
+static void mem_swap(void* a, void* b, size_t size, void* temp_buff) {
+	memcpy(temp_buff, a, size);
+	memcpy(a, b, size);
+	memcpy(b, temp_buff, size);
+}
+
+static void fix_down(ITEM_TYPE* base, int n, int width, int i,  compare_t compare)
 {
-	int i;
-	// Формируем нижний ряд пирамиды
-	for (i = (nel / 2) - 1; i >= 0; i--)
-		fixDown(a, i, nel);
-	// Просеиваем через пирамиду остальные элементы
-	for (i = nel - 1; i >= 1; i--)
-	{
-		swap(&a[0], &a[i]);
-		/*int temp = a[0];
-		a[0] = a[i];
-		a[i] = temp;*/
-		fixDown(a, 0, i - 1);
+	char* bytes = (char*)base;
+	int l, r, largest;
+	l = HEAP_LEFT(i);
+	r = HEAP_RIGHT(i);
+//	if (l < n && compare(base[l], base[i]) > 0)
+	if (l < n && compare(&bytes[l * width], &bytes[i * width]) > 0)
+		largest = l;
+	else
+		largest = i;
+//	if (r < n && compare(base[r], base[largest]) > 0)
+	if (r < n && compare(&bytes[r * width], &bytes[largest * width]) > 0)
+		largest = r;
+	if (largest != i) {
+		mem_swap(bytes + largest * width, bytes + i * width, width, swap_buff);
+		fix_down(base, n, width, largest, compare);
 	}
 }
 
-#define ITEM_COUNT 4
-int main(int argc, char* argv[]) {
+static void build_heap(ITEM_TYPE* a, int n, int width, compare_t compare)
+{
 	int i;
-	int a[ITEM_COUNT] = { 5, 0, 2, 1 };
-	// Заполнение массива случайными числами
-	//for (i = 0; i<ITEM_COUNT; i++)
-	//  a[i] = rand() % 20 - 10;
-	// Вывод элементов массива до сортировки
-	for (i = 0; i<ITEM_COUNT; i++)
-		printf("%d ", a[i]);
-	printf("\n");
-	heapSort(a, ITEM_COUNT); // вызов функции сортировки
-	// Вывод элементов массива после сортировки
-	for (i = 0; i<ITEM_COUNT; i++)
-		printf("%d ", a[i]);
-	printf("\n");
+	for (i = (n >> 1) - 1; i >= 0; i--) {
+		fix_down(a, n, width, i, compare);
+	}
+}
+
+//static void hsort(void* base, int nel, int width, compare_t compare)
+static void hsort(ITEM_TYPE* base, int nel, int width, compare_t compare)
+{
+	int i;
+	build_heap(base, nel, width, compare);
+	for (i = nel - 1; i > 0; i--) {
+	//	str_swap(&base[0], &base[i]);
+		mem_swap(base, (char*)base + i * width, width, swap_buff);
+		nel--;
+		fix_down(base, nel, width, 0, compare);
+	}
+}
+
+int main(int argc, char* argv[]) {
+	const size_t MAX_BUF_LEN = 512;
+	int len, slen, i;
+	char buffer[MAX_BUF_LEN];
+	
+	scanf("%d", &len);
+	if (len <= 0) {
+		fprintf(stderr, "length must be positive value\n");
+		return 1;
+	}
+
+	// похоже, что данные введены корректно, выделяем память под исходный массив
+	char** a = (char**)malloc(len * sizeof(char*));
+	if (a == NULL) {
+		fprintf(stderr, "failed to allocate memory for source array\n");
+		return 2;
+	}
+	
+	int error = 0;
+	// заполняем его из стандартного потока ввода
+	for (i = 0; i < len; i++) {
+		//printf("input a[%d]: ", i);
+		scanf("%511s", buffer);
+		slen = strlen(buffer);
+		a[i] = "";
+		if(slen) {
+			a[i] = (char*)malloc((slen + 1) * sizeof(char));
+			if(a[i] == NULL) {
+				error = i + 1;
+				break;
+			}
+			strcpy(a[i], buffer);
+		}
+	}
+	if (error == 0) {
+
+		//print_array(a, len, "input: ");
+		hsort(a, len, sizeof(ITEM_TYPE), compare_a_count);
+		
+		// выводим результат
+		print_array(a, len, "");
+
+	} else {
+		fprintf(stderr, "failed to allocate memory for a[%d]\n", error - 1);
+		return 3;
+	}
+	
+	// освобождаем память исходного массива
+	for (i = 0; i < len; i++)
+		if(a[i])
+			free(a[i]);
+	free(a);
+
 	return 0;
 }
