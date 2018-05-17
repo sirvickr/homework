1f374459d2dd2a10ae0704c00e69721fe2211c31
Виктор Краснов <vickr@stc-spb.ru>
2018-05-17 13:38:41 +0300

    * 859014 (C sorting): hsort: переход к PVOID: шаг 2 (окончательный вариант с комментариями)

--- 859014-c-sorting/hsort/hsort.c
+++ 859014-c-sorting/hsort/hsort.c
@@ -2,16 +2,22 @@
 #include <string.h>
 #include <stdlib.h>
 
+// для других типов нужны другие функции сравнения и вывода
 #define ITEM_TYPE char*
+
+// сортирующее дерево - пирамида, или куча (heap), хранится в массиве
+// эти макросы служат для навигации вниз и вверх по дереву
 #define HEAP_PARENT( i )	( ( ( ( i ) + 1 ) >> 1 ) - 1 )
 #define HEAP_LEFT( i )		( ( ( ( ( i ) + 1 ) << 1 ) + 0 ) - 1 )
 #define HEAP_RIGHT( i )		( ( ( ( ( i ) + 1 ) << 1 ) + 1 ) - 1 )
 
 typedef int (*compare_t)(const void* a, const void* b);
 
-static char swap_buff[sizeof(ITEM_TYPE)];
+// небольшая оптимизация для универсальной операции swap
+static char* swap_buff = NULL;
 
-static void print_array(char **a, const size_t len, const char *legend) {
+// вывод массива строк в консоль
+static void print_string_array(char **a, const size_t len, const char *legend) {
 	size_t i;
 	printf("%s", legend);
 	for (i = 0; i < len; i++)
@@ -19,6 +25,7 @@ static void print_array(char **a, const size_t len, const char *legend) {
 	printf("\n");
 }
 
+// подсчитать количество символов 'a' в строке
 static int calc_a_count(const char* s) {
 	int i, count = 0, slen = strlen(s);
 	for(i = 0; i < slen; i++) {
@@ -29,51 +36,54 @@ static int calc_a_count(const char* s) {
 	return count;
 }
 
+// сравнить количество символов 'a' в двух строках
 static int compare_a_count(const void* a, const void* b) {
 	return calc_a_count(*(const char**)a) - calc_a_count(*(const char**)b);
 }
 
+// поменять местами два блока памяти
 static void mem_swap(void* a, void* b, size_t size, void* temp_buff) {
 	memcpy(temp_buff, a, size);
 	memcpy(a, b, size);
 	memcpy(b, temp_buff, size);
 }
 
-static void fix_down(ITEM_TYPE* base, int n, int width, int i,  compare_t compare)
+// спусить i-й элемент пирамиды в надлежащее его величине место
+static void fix_down(void* base, int nel, int width, int i, compare_t compare)
 {
 	char* bytes = (char*)base;
 	int l, r, largest;
 	l = HEAP_LEFT(i);
 	r = HEAP_RIGHT(i);
-	if (l < n && compare(&bytes[l * width], &bytes[i * width]) > 0)
+	if (l < nel && compare(&bytes[l * width], &bytes[i * width]) > 0)
 		largest = l;
 	else
 		largest = i;
-	if (r < n && compare(&bytes[r * width], &bytes[largest * width]) > 0)
+	if (r < nel && compare(&bytes[r * width], &bytes[largest * width]) > 0)
 		largest = r;
 	if (largest != i) {
 		mem_swap(bytes + largest * width, bytes + i * width, width, swap_buff);
-		fix_down(base, n, width, largest, compare);
+		fix_down(base, nel, width, largest, compare);
 	}
 }
 
-static void build_heap(ITEM_TYPE* a, int n, int width, compare_t compare)
+// построить неубывающую пирамиду
+static void build_heap(void* base, int nel, int width, compare_t compare)
 {
 	int i;
-	for (i = (n >> 1) - 1; i >= 0; i--) {
-		fix_down(a, n, width, i, compare);
+	for (i = (nel >> 1) - 1; i >= 0; i--) {
+		fix_down(base, nel, width, i, compare);
 	}
 }
 
-//static void hsort(void* base, int nel, int width, compare_t compare)
-static void hsort(ITEM_TYPE* base, int nel, int width, compare_t compare)
+// функция пирамидальной сортировки
+static void hsort(void* base, int nel, int width, compare_t compare)
 {
 	int i;
 	build_heap(base, nel, width, compare);
 	for (i = nel - 1; i > 0; i--) {
 		mem_swap(base, (char*)base + i * width, width, swap_buff);
-		nel--;
-		fix_down(base, nel, width, 0, compare);
+		fix_down(base, --nel, width, 0, compare);
 	}
 }
 
@@ -82,13 +92,14 @@ int main(int argc, char* argv[]) {
 	int len, slen, i;
 	char buffer[MAX_BUF_LEN];
 	
+	// прочитать количество строк из стандартного потока ввода
 	scanf("%d", &len);
 	if (len <= 0) {
 		fprintf(stderr, "length must be positive value\n");
 		return 1;
 	}
 
-	// похоже, что данные введены корректно, выделяем память под исходный массив
+	// выделить память под исходный массив
 	char** a = (char**)malloc(len * sizeof(char*));
 	if (a == NULL) {
 		fprintf(stderr, "failed to allocate memory for source array\n");
@@ -96,7 +107,7 @@ int main(int argc, char* argv[]) {
 	}
 	
 	int error = 0;
-	// заполняем его из стандартного потока ввода
+	// заполнить его из стандартного потока ввода, выделяя память для каждоой строки
 	for (i = 0; i < len; i++) {
 		//printf("input a[%d]: ", i);
 		scanf("%511s", buffer);
@@ -111,13 +122,20 @@ int main(int argc, char* argv[]) {
 			strcpy(a[i], buffer);
 		}
 	}
+	// выполнить пирамидальную сортировку и вывести результат
 	if (error == 0) {
 
-		//print_array(a, len, "input: ");
+		//print_string_array(a, len, "input: ");
+
+		// чтобы не выделять каждый раз для операции swap()
+		swap_buff = (char*)malloc(sizeof(ITEM_TYPE));
+		
 		hsort(a, len, sizeof(ITEM_TYPE), compare_a_count);
 		
+		free(swap_buff);
+		
 		// выводим результат
-		print_array(a, len, "");
+		print_string_array(a, len, "");
 
 	} else {
 		fprintf(stderr, "failed to allocate memory for a[%d]\n", error - 1);
