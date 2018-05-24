441067d07ffb7efb141e79cd83b3dfe22ef78d85
sirvickr <sirvickr@gmail.com>
2018-05-25 00:38:47 +0300

    * 868701 лаба 4 (XorCipher): добавил измерение времени гаммирования и объединил два пункта меню в один

--- 868701-C++/XorCipher/XorCipher.cpp
+++ 868701-C++/XorCipher/XorCipher.cpp
@@ -35,43 +35,6 @@ size_t inputCount(const string& prompt) {
 	cin >> result;
 	return result;
 }
-/*
-// Вывод последовательности чисел на экран (со статистикой) и в файл
-void writeNumbers(size_t m, const vector<uint32_t>& numbers, const string& fileName) {
-	if (numbers.empty())
-		return;
-
-	if (numbers.size() <= 15) // пожалеем консоль (результат увидим в файле)
-		printSequence(numbers, "ПСП:");
-
-	vector<double> stats(100, 0);
-	const size_t intervalLength = size_t(double(m) / double(stats.size()) + 0.5);
-	// Подсчёт и вывод статистики
-	for (auto number : numbers)
-		stats[number / intervalLength] += 1;
-	for (size_t i = 0; i < 100; i++)
-		if (stats[i] > 0.0)
-			cout << "[" << i << "]\t" << fixed << setprecision(2) << stats[i] * 100.0 / numbers.size() << "%" << endl;
-
-	// Запись в файл
-	ofstream output(fileName);
-	if (!output) {
-		cerr << "Не удалось открыть файл \"" << fileName << "\"" << endl;
-		return;
-	}
-
-	for (auto number : numbers)
-		output << number << "\n";
-
-	cerr << "Последовательность записана в файл \"" << fileName << "\"" << endl;
-}
-
-void generateNumbers(generator_t& gen, const string& fileName) {
-	size_t count = inputCount("Введите размер псевдослучайной последовательности: ");
-	if (count) {
-		writeNumbers(gen.M(), gen.NextNumbers(count, true), fileName);
-	}
-}*/
 
 // Запись параметров ГПСЧ в файл
 void writeParams(const generator_t& gen, const string& fileName) {
@@ -105,33 +68,37 @@ void processFile(generator_t& gen, const string& dataFileName, const string& key
 	{
 		ifstream input(dataFileName, ios::binary);
 		if (!input) {
-			cerr << "не удалось открыть файл \"" << dataFileName << "\" на чтение" << endl;
+			cerr << "Не удалось открыть файл \"" << dataFileName << "\" на чтение" << endl;
 			return;
 		}
 		input.seekg(0, ios::end);
 		size = static_cast<size_t>(input.tellg());
-		cout << "data size " << size << endl;
+		cout << "Размер файла данных: " << size << endl;
 		input.seekg(0, ios::beg);
 		if (!size) {
-			cerr << "файл \"" << dataFileName << "\" пуст" << endl;
+			cerr << "Файл \"" << dataFileName << "\" пуст" << endl;
 			return;
 		}
 		lpBuf = new char[size];
 		input.read(lpBuf, size);
 	}
+
+	using namespace std::chrono;
+	// сохраняем текущее время
+	auto start = chrono::steady_clock::now();
+
 	// генерируем псевдослучайную последовательность нужной длины для гаммирования
 	// размер данных в байтах (8 бит), а длина ПСП в numbers_t (32 бита)
-	generator_t::numbers_t gamma = gen.NextNumbers((size - 1) / sizeof(generator_t::number_t) + 1, true);
-	cout << "gamma size " << gamma.size() << ":" << uppercase << setfill('0');
-	for (auto word : gamma)
-		cout << " " << hex << setw(8) << word;
-	cout << dec << endl;
+	auto gamma = gen.NextNumbers((size - 1) / sizeof(generator_t::number_t) + 1, true);
 	for (uint32_t i = 0; i < size; ++i) { // цикл по каждому байту исходных данных
-		// выбеляем соответствующий байт из псевдослучайной последовательности
-		//char g = gamma[i / sizeof(generator_t::number_t)] >> (i << 3);
+		// выделяем соответствующий байт из псевдослучайной последовательности
 		lpBuf[i] ^= gamma[i / sizeof(generator_t::number_t)] >> (i << 3);
-		//cout << "data " << hex << setw(2) << int(lpBuf[i]) << " gamma " << setw(2) << int(g) << ": " << setw(2) << int(g ^ lpBuf[i]) << endl;
 	}
+
+	// сохраняем текущее и вычисляем затраченное на обработку время
+	auto finish = steady_clock::now();
+	cout << "Время гаммирования: " << duration_cast<microseconds>(finish - start).count() << " мкс" << endl;
+
 	// записываем данные в тот же файл
 	ofstream output(dataFileName, ios::binary);
 	if (!output) {
@@ -151,7 +118,7 @@ int main()
 	// объект генератора
 	generator_t gen(19997, 55529, 347675);
 	// название файла с данными для гаммирования
-	string dataFileName = "data.txt";
+	string dataFileName = "1.txt";
 	// название файла для хранения параметров ГПСЧ
 	string keyFileName = "params.key";
 
@@ -165,9 +132,8 @@ int main()
 		cout << "[0] Завершение работы" << endl;
 		cout << "[1] Указать файл данных ( " << dataFileName << " )" << endl;
 		cout << "[2] Указать файл параметров ГПСЧ ( " << keyFileName << " )" << endl;
-		cout << "[3] Сгенерировать параметры ГПСЧ" << endl;
-		cout << "[4] Сохранить параметры ГПСЧ в файл" << endl;
-		cout << "[5] Выполнить гаммирование данных в файле" << endl;
+		cout << "[3] Сгенерировать новые параметры ГПСЧ" << endl;
+		cout << "[4] Выполнить гаммирование данных в файле" << endl;
 		cout << "\nВаш выбор: ";
 		cin.sync();
 		cin >> menu;
@@ -185,11 +151,9 @@ int main()
 		case 3:
 			gen.createParams();
 			cout << "Сформированы параметры: A " << gen.A() << " B " << gen.B() << " C0 " << gen.Seed() << endl;
-			break;
-		case 4:
 			writeParams(gen, keyFileName);
 			break;
-		case 5:
+		case 4:
 			processFile(gen, dataFileName, keyFileName);
 			break;
 		default:
