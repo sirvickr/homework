4776bb3e43d463a7f9274abd9dfdab18119e5673
sirvickr <sirvickr@gmail.com>
2018-06-25 16:30:21 +0300

    + 882956 (C++ и pascal): pas

---
+++ 882956-c-pascal/list.pas
@@ -0,0 +1,123 @@
+﻿{Вводим новый тип данных}
+Type BT = LongInt;
+Type U = ^Zveno;
+Zveno = Record Inf : BT; Next: U End;
+{Проверка на пустой элемент - нулевой адрес}
+Function Pust(First : U) : Boolean;
+Begin
+  Pust := First = Nil
+End;
+{Процедура добавления элемента в начало списка; в x содержится добавляемая информация}
+Procedure V_Nachalo(Var First : U; X : BT);
+Var Vsp : U;
+Begin
+  New(Vsp);
+  Vsp^.Inf := X;
+  {Тот элемент, что был заглавным, становится вторым по счѐту}
+  Vsp^.Next := First;
+  First := Vsp; {Новый элемент становится заглавным}
+End;
+{Процедура удаления элемента из начала списка; в x содержится информация из удалѐнного звена}
+Procedure Iz_Nachala(Var First : U; Var X : BT);
+Var Vsp : U;
+Begin
+  Vsp := First; {Забираем ссылку на текущее заглавный элемент}
+  First := First^.Next; {Второй элемент становится заглавным}
+  X := Vsp^.Inf; {Забираем информацию из удаляемого элемента}
+  Dispose(Vsp); {Уничтожаем элемент}
+End;
+{Процедура добавления элемента в конец списка}
+Procedure V_Konez(Var First : U; X : BT);
+Var Vsp, Tmp : U;
+Begin
+  New(Vsp); {Создаем пустый элемент}
+  Vsp^.Inf := X; {Заносим информацию}
+  Vsp^.Next:= Nil; {Обнуляем ссылку}
+  {Если список был пустой - включаем первый элемент}
+  If First = Nil Then First := Vsp
+  Else
+  Begin
+    Tmp := First; {Проходим до конца списка}
+    While Not Pust(Tmp^.Next) Do Tmp := Tmp^.Next;
+    Tmp^.Next := Vsp; {Вставляем элемент последним}
+  End;
+End;
+{Процедура удаления элемента из конца списка}
+Procedure Iz_Konza(Var First : U; Var X : BT);
+Var Vsp, Tmp : U;
+Begin
+  Tmp := First;
+  Vsp := First;
+  {Если список состоит из одного элемента}
+  If First^.Next = Nil Then First := Nil
+  Else
+  Begin
+    {Проходим до предпоследнего элемента}
+    While Not Pust(Tmp^.Next^.Next) Do Tmp := Tmp^.Next;
+    Vsp := Tmp^.Next; {Запоминаем адрес удаляемого элемента}
+    Tmp^.Next := Nil; {Определяем новый конец списка}
+  End;
+  X := Vsp^.Inf; {Забираем информацию из удаляемого звена}
+  Dispose(Vsp); {Уничтожаем элемент}
+End;
+{Процедура печати списка}
+Procedure Print(First : U);
+Begin
+  If First = Nil Then writeln('Пустой список!')
+  Else
+  Begin
+    While First <> Nil Do
+    Begin
+      Write(First^.Inf : 6);
+      First := First^.Next
+    End;
+  End;
+  WriteLn
+End;
+{основная программа - вызовы функций}
+var chislo : BT;
+Var L1, L2, L3 : U;
+Var First : U;
+{Var Vsp, Tmp : U;}
+Begin
+  L1 := Nil;
+  L2 := Nil;
+  L3 := Nil;
+  {Вводим первый список}
+  writeln('Ввод первого списка.');
+  writeln('вводи числа(0 - конец ввода)');
+  read(chislo);
+  While chislo <> 0 Do
+  Begin
+    V_Konez(L1, chislo);
+    read(chislo);
+  End;
+  writeln('Первый список:');
+  Print(L1);
+  {Вводим второй список}
+  writeln('Ввод второго списка.');
+  writeln('вводи числа(0 - конец ввода)');
+  read(chislo);
+  While chislo <> 0 Do
+  Begin
+    V_Konez(L2, chislo);
+    read(chislo);
+  End;
+  writeln('Второй список:');
+  Print(L2);
+  {Объединяем списки, создавая на их основе третий список}
+  First := L1;
+  While First <> Nil Do
+  Begin
+    V_Konez(L3, First^.Inf);
+    First := First^.Next
+  End;
+  First := L2;
+  While First <> Nil Do
+  Begin
+    V_Konez(L3, First^.Inf);
+    First := First^.Next
+  End;
+  writeln('Третий список:');
+  Print(L3);
+End.
---
+++ 882956-c-pascal/tree.pas
@@ -0,0 +1,124 @@
+﻿{Вводим новый тип данных}
+type ref = ^node;
+node = record
+  key, count: integer;
+  left, right: ref;
+end;
+{Печать содержимого бинарного дерева}
+procedure Print_Btree (w: ref; l: integer);
+var i : integer;
+begin
+  if (w <> Nil) then
+  with w^ do
+  begin
+    Print_Btree(right, l + 1);
+    for i := 1 to l * 4 do
+      write(' ');
+      //writeln('значение = ',key,' счетчик = ', count);
+    writeln(key);
+    Print_Btree(left, l + 1);
+  end;
+end;
+{Построение дерева повторений}
+procedure Include (x: integer; var p:ref);
+begin
+  if p = Nil then
+  begin {Добавляем вершину}
+    new (p);
+    with p^ do
+    begin
+      key := x;
+      count := 1;
+      left := Nil;
+      right := Nil;
+    end;
+  end
+  else
+  begin
+    if x = p^.key then
+      {Если число есть в дереве, то увеличиваем счѐтчик}
+      p^.count := p^.count + 1
+    else
+      if x > p^.key then
+        {Добавляем в правое поддерево}
+        Include(x, p^.right)
+      else
+        {Добавляем в левое поддерево}
+        Include(x, p^.left);
+  end;
+end;
+{Построение идеально-сбалансированного дерева с n вершинами}
+function tree (n : integer): ref;
+var
+newnode : ref;
+x, l, r : integer;
+begin
+  if n = 0 then tree := nil else
+  begin
+    l := n div 2; r := n - l - 1;
+    read(x);
+    {Добавляем вершину}
+    new (newnode);
+    with newnode^ do
+    begin
+      key := x;
+      left := tree(l);
+      right := tree(r);
+    end;
+    tree := newnode;
+  end;
+end;
+{Построение дерева повторений}
+procedure Swap_Max_Min (var btree: ref);
+var
+  pmin, pmax: ref;
+  value, count: integer;
+begin
+  if btree <> Nil then
+  begin
+    {спускаемся к минимальному элементу}
+    pmin := btree;
+    while true Do
+    begin
+      if pmin^.left = Nil then
+        break;
+      pmin := pmin^.left;
+    end;
+    {спускаемся к максимальному элементу}
+    pmax := btree;
+    while true Do
+    begin
+      if pmax^.right = Nil then
+        break;
+      pmax := pmax^.right;
+    end;
+    {swap}
+    value := pmax^.key;
+    count := pmax^.count;
+    pmax^.key := pmin^.key;
+    pmax^.count := pmin^.count;
+    pmin^.key := value;
+    pmin^.count := count;
+  end;
+end;
+{Основная программа}
+var
+root: ref;
+k: integer;
+begin
+  { возможная тестовая последовательность: 3, -1, 5, 9, 8, -7, -5, 2, 1, 4 }
+  root := Nil;
+  writeln('Дерево повторений.');
+  writeln('Вводи числа (0 - конец ввода)');
+  read(k);
+  while k <> 0 Do
+  begin
+    Include (k,root);
+    read(k);
+  end;
+  writeln('Печать дерева');
+  Print_Btree (root,2);
+  Swap_Max_Min(root);
+  writeln('Печать дерева (после смены мест максималльного и минимального элементов)');
+  Print_Btree (root,2);
+end.
