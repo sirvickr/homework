44e67c0ed8e65e8d2a56853f689b0620bc2f8fb7
sirvickr <sirvickr@gmail.com>
2019-06-06 20:41:01 +0300

    * 1097376 C# (StackCalc) working version with remarks

---
+++ 1097376 C#/StackCalc/Calc.cs
@@ -0,0 +1,223 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace StackCalc
+{
+    
+    /*
+     * Класс, отвечающий за основные функции программы - 
+     * управление стеком, хранение параментров и выполнение операций
+     */
+
+    class Calc
+    {
+
+        Stack<string> stack = new Stack<string>();
+        Dictionary<string, double> defs = new Dictionary<string, double>();
+
+        // Задать значение сохраняемого параметра
+        // параметры функции:
+        // name - имя сохраняемого параметра
+        // str - строковое значение сохраняемого параметра
+        public void Define(string name, string str)
+        {
+            // Преобразовать строковое значение 
+            // в число с плавающей точкой
+            double value = double.Parse(str.Trim(),
+                CultureInfo.InvariantCulture.NumberFormat);
+            if (defs.ContainsKey(name))
+            {
+                // Заменить существующее значение
+                defs[name] = value;
+            }
+            else
+            {
+                // Добавить новое определение
+                defs.Add(name, value);
+            }
+        }
+
+        // Поместить операнд на вершину стека
+        // параметры функции:
+        // arg - строковое представление операнда
+        public void Push(string arg)
+        {
+            stack.Push(arg);
+        }
+
+        // Снять операнд с вершины стека и напечать
+        public void Pop()
+        {
+            if (stack.Count > 0)
+            {
+                Console.WriteLine(GetValue(stack.Pop()).ToString());
+            }
+            else
+            {
+                Console.WriteLine("Стек пуст");
+            }
+        }
+
+        // Напечатать значение операнда на вершине стека
+        // если на вершине стека находится имя сохраённого
+        // параметра, то его значение извлекается из хранилища
+        public void Print()
+        {
+            if (stack.Count > 0)
+            {
+                Console.WriteLine(GetValue(stack.Peek()).ToString());
+            }
+            else
+            {
+                Console.WriteLine("Стек пуст");
+            }
+        }
+
+        // Складывает два верхних операнда,
+        // результат помещает обратно в стек
+        public void Add()
+        {
+            if (stack.Count > 1)
+            {
+                // Забрать из стека первый операнд
+                double a = GetValue(stack.Pop());
+                // Забрать из стека второй операнд
+                double b = GetValue(stack.Pop());
+                // Выполнить операцию (сложение)
+                double c = a + b;
+                // Поместить результат обратно в стек
+                stack.Push(c.ToString());
+            }
+            else
+            {
+                Console.WriteLine("Недостаточно аргументов для операции");
+            }
+        }
+
+        // Вычитает два верхних операнда,
+        // результат помещает обратно в стек
+        public void Sub()
+        {
+            if (stack.Count > 1)
+            {
+                // Забрать из стека первый операнд
+                double a = GetValue(stack.Pop());
+                // Забрать из стека второй операнд
+                double b = GetValue(stack.Pop());
+                // Выполнить операцию (вычитание)
+                double c = a - b;
+                // Поместить результат обратно в стек
+                stack.Push(c.ToString());
+            }
+            else
+            {
+                Console.WriteLine("Недостаточно аргументов для операции");
+            }
+        }
+
+        // Перемножает два верхних операнда,
+        // результат помещает обратно в стек
+        public void Mul()
+        {
+            if (stack.Count > 1)
+            {
+                // Забрать из стека первый операнд
+                double a = GetValue(stack.Pop());
+                // Забрать из стека второй операнд
+                double b = GetValue(stack.Pop());
+                // Выполнить операцию (умножение)
+                double c = a * b;
+                // Поместить результат обратно в стек
+                stack.Push(c.ToString());
+            }
+            else
+            {
+                Console.WriteLine("Недостаточно аргументов для операции");
+            }
+        }
+
+        // Делит два верхних операнда,
+        // результат помещает обратно в стек
+        public void Div()
+        {
+            if (stack.Count > 1)
+            {
+                // Забрать из стека первый операнд
+                double a = GetValue(stack.Pop());
+                // Забрать из стека второй операнд
+                double b = GetValue(stack.Pop());
+                // Выполнить операцию (деление)
+                double c = a / b;
+                // Поместить результат обратно в стек
+                stack.Push(c.ToString());
+            }
+            else
+            {
+                Console.WriteLine("Недостаточно аргументов для операции");
+            }
+        }
+
+        // Извлекает корень из операнда на верхушине стека,
+        // результат помещает обратно в стек
+        public void Sqrt()
+        {
+            if (stack.Count > 0)
+            {
+                // Забрать из стека аргумент
+                double x = GetValue(stack.Pop());
+                if(x >= 0)
+                {
+                    // Выполнить операцию (взятие квадратного корня)
+                    x = Math.Sqrt(x);
+                }
+                else
+                {
+                    Console.WriteLine("Невозможно извлечь квадратный корень из отрицательного числа");
+                }
+                // Поместить результат обратно в стек
+                stack.Push(x.ToString());
+            }
+            else
+            {
+                Console.WriteLine("Недостаточно аргументов для операции");
+            }
+        }
+
+        // Возвращает действительное число, соответствующее
+        // переданному строковому аргументу.
+        // Если это число, то функция преобразует строковое
+        // представление в число с плавающей точкой, иначе
+        // извлекает число из хранилища именованных параметров,
+        // параметры функции:
+        // arg - строковое представление операнда
+        private double GetValue(string arg)
+        {
+            double value = 0.0;
+            try
+            {
+                // Пробуем преобразовать строковый аргумент в число с плавающей точкой
+                value = double.Parse(arg.Trim(),
+                    CultureInfo.InvariantCulture.NumberFormat);
+            }
+            catch (System.FormatException)
+            {
+                // Ошибка - возможно, это имя параметра, 
+                // пробуем найти его в коллекции определений
+                if (defs.ContainsKey(arg))
+                {
+                    // Заменить существующее значение
+                    value = defs[arg];
+                }
+                else
+                {
+                    // Ничего не получилось - выдать ошибку
+                    throw new System.Exception("Некорректный аргумент: " + arg);
+                }
+            }
+            return value;
+        }
+
+    }
+
+}
--- 1097376 C#/StackCalc/Program.cs
+++ 1097376 C#/StackCalc/Program.cs
@@ -1,15 +1,183 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 
 namespace StackCalc
 {
+
+    // Основной класс приложения
     class Program
     {
+
+        // Ссылка на класс калькулятора
+        static Calc calc = new Calc();
+
+        // Основная функця (точка входа) приложения
         static void Main(string[] args)
         {
+            Console.WriteLine("Программа-калькулятор");
+            try
+            {
+                // Проверяем наличие аргументов командной строки
+                if (args.Length > 0)
+                {
+                    // Первый аргумент считаем именем файла,
+                    // считываем команды из него
+                    string fileName = args[0];
+                    Console.WriteLine("Ввод из файла \"" + fileName + "\"");
+                    // Открываем файл на чтение
+                    StreamReader reader = new StreamReader(fileName);
+                    // Читаем строки до конца файла
+                    while (true)
+                    {
+                        string cmd = reader.ReadLine();
+                        if (cmd != null)
+                        {
+                            // Каждую прочитанную строку печатаем
+                            // и отправляем в калькулятор
+                            if (processCommand(cmd, true))
+                            {
+                                break;
+                            }
+                        }
+                        else
+                        {
+                            break;
+                        }
+                    }
+                    // Закрываем файл
+                    reader.Close();
+                }
+                else
+                {
+                    // Программа запущена без параментров,
+                    // считываем команды из консоли
+                    Console.WriteLine("Ввод с клавиатуры");
+                    while (true)
+                    {
+                        // Каждую прочитанную строку передаём
+                        // калькулятору без вывода на экран
+                        if (processCommand(Console.ReadLine(), false))
+                        {
+                            break;
+                        }
+                    }
+                }
+            }
+            catch (System.Exception exception)
+            {
+                Console.WriteLine("Ошибка: "+ exception.Message);
+            }
         }
+
+        // Функция обработки отдельной команды
+        // Возвражаемое значение:
+        // false - продолжаем ввод команд
+        // true - завершаем работу
+        static bool processCommand(string cmd, bool trace)
+        {
+            // Комментарии и пустые строки пропускаем
+            if (cmd.Length == 0 || cmd[0] == '#')
+            {
+                return false;
+            }
+
+            // Выводим исходную команду на консоль
+            // (при вводе с клавиатуры не имеет смысла,
+            // т.к. пользователь уже её сам набрал в консоли)
+            if (trace)
+            {
+                Console.WriteLine(cmd);
+            }
+
+            // Переводим все символы в верхний регистр
+            // (убираем зависимость от регистра)
+            cmd = cmd.ToUpper();
+
+            // Разбиваем команду на имя команды 
+            // и аргументы (если есть)
+            string[] tokens = cmd.Split(new char[] { ' ' });
+
+            // Первый элемент обязан содержать имя команды
+            if (tokens.Length == 0)
+            {
+                return false;
+            }
+
+            if (tokens[0] == "DEFINE")
+            {
+                // Задать значение параметра
+                // (ожидаем: DEFINE имя значение)
+                if (tokens.Length == 3)
+                {
+                    try
+                    {
+                        calc.Define(tokens[1], tokens[2]);
+                    }
+                    catch (System.FormatException exeption)
+                    {
+                        Console.WriteLine("Formatxception: " + 
+                            exeption.Message);
+                    }
+                }
+            }
+            else if (tokens[0] == "PUSH")
+            {
+                // Положить число на стек
+                // (ожидаем: PUSH значение)
+                if (tokens.Length == 2)
+                {
+                    calc.Push(tokens[1]);
+                }
+            }
+            else if (cmd == "POP")
+            {
+                // Снять число со стека
+                calc.Pop();
+            }
+            else if (cmd == "PRINT")
+            {
+                // Печатаем верхушку стека
+                calc.Print();
+            }
+            else if (cmd == "+")
+            {
+                // Складываем два верхних операнда
+                calc.Add();
+            }
+            else if (cmd == "-")
+            {
+                // Вычитаем два верхних операнда
+                calc.Sub();
+            }
+            else if (cmd == "*")
+            {
+                // Перемножаем два верхних операнда
+                calc.Mul();
+            }
+            else if (cmd == "/")
+            {
+                // Делим два верхних операнда
+                calc.Div();
+            }
+            else if (cmd == "SQRT")
+            {
+                // Извлекаем корень из операнда на верхушине стека
+                calc.Sqrt();
+            }
+            else if (cmd == "END")
+            {
+                // Возвращаем признак завершения работы
+                return true;
+            }
+            else
+            {
+                // Сообщаем об ошибке, но не прерываем работу
+                Console.WriteLine("Неизвестная команда: \"" + cmd + "\"");
+            }
+            // Возвращаем признак продолжения работы
+            return false;
+        }
+
     }
+
 }
