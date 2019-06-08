3fcf7e194897c228b5d21fd7d0b2761ccc2a3ea8
sirvickr <sirvickr@gmail.com>
2019-06-08 13:03:23 +0300

    * 1062409 (WinAPI SysInfo)L release

--- 1062409 C++ SysInfo/SysInfo/SysInfo.cpp
+++ 1062409 C++ SysInfo/SysInfo/SysInfo.cpp
@@ -14,13 +14,40 @@ int main()
 	// Отобразить общую аппаратную информацию
 	GetSystemInfo(&sysInfo);
 	_tprintf(TEXT("Hardware information: \n"));
-	_tprintf(TEXT("  OEM ID: %u\n"), sysInfo.dwOemId);
-	_tprintf(TEXT("  Number of processors: %u\n"), sysInfo.dwNumberOfProcessors);
-	_tprintf(TEXT("  Page size: %u\n"), sysInfo.dwPageSize);
-	_tprintf(TEXT("  Processor type: %u\n"), sysInfo.dwProcessorType);
-	_tprintf(TEXT("  Minimum application address: %lx\n"), sysInfo.lpMinimumApplicationAddress);
-	_tprintf(TEXT("  Maximum application address: %lx\n"), sysInfo.lpMaximumApplicationAddress);
-	_tprintf(TEXT("  Active processor mask: %u"), sysInfo.dwActiveProcessorMask);
+	_tprintf(TEXT(" OEM ID: %u (obsolete)\n"), sysInfo.dwOemId);
+	_tprintf(TEXT(" Processor architecture: %04X ("), sysInfo.wProcessorArchitecture);
+	switch (sysInfo.wProcessorArchitecture) {
+	case PROCESSOR_ARCHITECTURE_AMD64:
+		_tprintf(TEXT("x64 (AMD or Intel)"));
+		break;
+	case PROCESSOR_ARCHITECTURE_ARM:
+		_tprintf(TEXT("ARM"));
+		break;
+	case PROCESSOR_ARCHITECTURE_ARM64:
+		_tprintf(TEXT("ARM64"));
+		break;
+	case PROCESSOR_ARCHITECTURE_IA64:
+		_tprintf(TEXT("Intel Itanium"));
+		break;
+	case PROCESSOR_ARCHITECTURE_INTEL:
+		_tprintf(TEXT("x86"));
+		break;
+	case PROCESSOR_ARCHITECTURE_UNKNOWN:
+		_tprintf(TEXT("Unknown"));
+		break;
+	default:
+		_tprintf(TEXT("Unsupported value"));
+	}
+	_tprintf(TEXT(")\n"));
+	_tprintf(TEXT(" Number of processors: %u\n"), sysInfo.dwNumberOfProcessors);
+	_tprintf(TEXT(" Page size: %u\n"), sysInfo.dwPageSize);
+	_tprintf(TEXT(" Processor type: %u (obsolete)\n"), sysInfo.dwProcessorType);
+	_tprintf(TEXT(" Processor revision: %u (obsolete)\n"), sysInfo.wProcessorRevision);
+	_tprintf(TEXT(" Processor level: %u\n"), sysInfo.wProcessorLevel);
+	_tprintf(TEXT(" Low application address: %08X\n"), sysInfo.lpMinimumApplicationAddress);
+	_tprintf(TEXT(" High application address: %08X\n"), sysInfo.lpMaximumApplicationAddress);
+	_tprintf(TEXT(" Active processor mask: %08X\n"), sysInfo.dwActiveProcessorMask);
+	_tprintf(TEXT(" Allocation granularity: %u\n"), sysInfo.dwAllocationGranularity);
 
 	// Отобразить имя компьютера
 	if (!GetComputerName(infoBuf, &bufCharCount)) {
