a08343e869f1617b0e1ce2cb8872d9d13b5f6fcc
sirvickr <sirvickr@gmail.com>
2019-06-23 18:57:56 +0300

    1107058 (C, matrix): release

--- 1107058 С (matrix)/matrix/matrix.cpp
+++ 1107058 С (matrix)/matrix/matrix.cpp
@@ -6,46 +6,18 @@
 #define B_SIZE 7
 #define C_SIZE 4
 
-int testA[A_SIZE][A_SIZE] = {
-	{ 1, 1, 1, 1, 1 },
-	{ 1, 1, 1, 1, 1 },
-	{ 1, 1, 1, 1, 1 },
-	{ 1, 1, 1, 1, 1 },
-	{ 1, 1, 1, 1, 1 },
-};
-int testB[B_SIZE][B_SIZE] = {
-	{ 1, 1, 1, 1, 1, 1, 1 },
-	{ 1, 1, 1, 1, 1, 1, 1 },
-	{ 1, 1, 1, 1, 1, 1, 1 },
-	{ 1, 1, 1, 1, 1, 1, 1 },
-	{ 1, 1, 1, 1, 1, 1, 1 },
-	{ 1, 1, 1, 1, 1, 1, 1 },
-	{ 1, 1, 1, 1, 1, 1, 1 },
-};
-int testC[C_SIZE][C_SIZE] = {
-	{ 1, 1, 1, 1 },
-	{ 1, 1, 1, 1 },
-	{ 1, 1, 1, 1 },
-	{ 1, 1, 1, 1 },
-};
-
-/* Возвращает произведение положительных 
-   элементов главной диагонали квадратной матрицы
-   Параметры:
-   matrix - указатель на первый элемент массива, 
-            содержащего элементы квадратной матрицы
-   dimSize - количество строк/столбцов матрицы
-*/
+// Предварительное объявление функции
 int main_positive_product(int* matrix, int dimSize);
 
+// Основная функция - точка входа программы
 int main()
 {
-
+	// Выделение памяти под массивы для 
+	// хранения элементов исходных матриц
 	int* A = (int*)malloc(sizeof(int) * A_SIZE * A_SIZE);
 	int* B = (int*)malloc(sizeof(int) * B_SIZE * B_SIZE);
 	int* C = (int*)malloc(sizeof(int) * C_SIZE * C_SIZE);
 
-#if 0
 	// Ввод матрицы A
 	printf("A[%d][%d]:\n", A_SIZE, A_SIZE);
 	for (int i = 0; i < A_SIZE; ++i)
@@ -68,6 +40,7 @@ int main()
 			scanf("%d", B + B_SIZE * i + j);
 		}
 	}
+	
 	// Ввод матрицы C
 	printf("C[%d][%d]:\n", C_SIZE, C_SIZE);
 	for (int i = 0; i < C_SIZE; ++i)
@@ -79,56 +52,36 @@ int main()
 			scanf("%d", C + C_SIZE * i + j);
 		}
 	}
-#else
-	for (int i = 0; i < A_SIZE; ++i)
-	{
-		for (int j = 0; j < A_SIZE; ++j)
-		{
-			A[A_SIZE * i + j] = testA[i][j];
-		}
-	}
-	// Ввод матрицы B
-	for (int i = 0; i < B_SIZE; ++i)
-	{
-		for (int j = 0; j < B_SIZE; ++j)
-		{
-			B[B_SIZE * i + j] = testB[i][j];
-		}
-	}
-	// Ввод матрицы C
-	for (int i = 0; i < C_SIZE; ++i)
-	{
-		for (int j = 0; j < C_SIZE; ++j)
-		{
-			C[C_SIZE * i + j] = testC[i][j];
-		}
-	}
-#endif
 
+	// Вычисляем произведения положительных элементов 
+	// главной диагонали каждой из исходных матриц
 	int P_A = main_positive_product(A, A_SIZE);
 	int P_B = main_positive_product(B, B_SIZE);
 	int P_C = main_positive_product(C, C_SIZE);
 
-	printf("A:");
-	for (int i = 0; i < A_SIZE * A_SIZE; ++i)
-	{
-		printf(" %d", A[i]);
-	}
-	printf("\n");
-
+	// Вычисляем результат выражения
 	int P = P_A + P_B - P_C;
 
+	// Выводим результат
 	printf("%d + %d - %d = %d\n", P_A, P_B, P_C, P);
 
+	// Ожидаем реакции пользователя
 	printf("Press any key to finish\n");
 	getchar();
 
-	// Освобождаем выделеннею память
+	// Освобождаем выделенную память
 	free(A);
 	free(B);
 	free(C);
 }
 
+/* Возвращает произведение положительных
+   элементов главной диагонали квадратной матрицы
+   Параметры:
+   matrix - указатель на первый элемент массива,
+			содержащего элементы квадратной матрицы
+   dimSize - количество строк/столбцов матрицы
+*/
 int main_positive_product(int* matrix, int dimSize)
 {
 	int result = 1;
