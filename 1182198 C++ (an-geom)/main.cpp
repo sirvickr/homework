de45ed610b1305fb02e4d113125125b141730adc
Виктор Краснов <vickr@inbox.ru>
2019-12-23 12:50:24 +0300

    1182198 C++ (geometry): измерил параметры фигуры

--- 1182198 C++ (an-geom)/main.cpp
+++ 1182198 C++ (an-geom)/main.cpp
@@ -4,17 +4,52 @@
 
 #include <iostream>
 #include <iomanip>
+#include <cmath>
 
 using namespace std;
 
 struct point {
-	double x = 0.0;
-	double y = 0.0;
-}
+	double x;
+	double y;
+};
 
-size_t count = 12;
+const size_t pointCount = 12;
 
-point points[] = {
+const size_t paramCount = 12;
+/* параметры фигуры:
+x содержит соотношение длин векторов,
+y содержит значение косинуса угла между векторами
+*/
+const point params[paramCount] = {
+	{ 2.23607,  0.447214 },
+	{       2,         0 },
+	{       3,         0 },
+	{ 3.60555,   -0.5547 },
+	{ 2.82843, -0.707107 },
+	{ 3.60555,  -0.83205 },
+ 	{       3,        -1 },
+ 	{       2,        -1 },
+	{ 2.23607, -0.894427 },
+	{       1,        -0 }
+};
+
+#if 1
+point points[pointCount] = {
+	{ 2.0, 2.0 }, // A
+	{ 1.0, 2.0 }, // B
+	{ 1.0, 4.0 }, // C
+	{ 2.0, 4.0 }, // D
+	{ 2.0, 5.0 }, // E
+	{ 4.0, 5.0 }, // F
+	{ 4.0, 4.0 }, // G
+	{ 5.0, 4.0 }, // H
+	{ 5.0, 2.0 }, // I
+	{ 4.0, 2.0 }, // J
+	{ 4.0, 1.0 }, // K
+	{ 2.0, 1.0 }, // L
+};
+#else
+point points[pointCount] = {
 	{  5.0,  4.0 }, // A
 	{  3.0,  4.0 }, // B
 	{  3.0,  8.0 }, // C
@@ -28,6 +63,7 @@ point points[] = {
 	{  9.0,  2.0 }, // K
 	{  5.0,  2.0 }, // L
 };
+#endif
 
 int main(int argc, char* argv[])
 {
@@ -37,8 +73,22 @@ int main(int argc, char* argv[])
 		cout << "or   " << argv[0] << " 65535" << endl;
 		return 0;
 	}*/
-	for(size_t i = 0; i < count; ++i) {
-		cout << setw(7) << points[i].x << setw(7) << points[i].y << "\t" << endl;
+	size_t i = 0;
+	
+	point v1 = { points[1].x - points[0].x, points[1].y - points[0].y };
+	double len1 = sqrt(v1.x * v1.x + v1.y * v1.y);
+	
+	for(size_t j = 2; j < pointCount; ++j) {
+		point v2 = { points[j].x - points[i].x, points[j].y - points[i].y };
+		double len2 = sqrt(v2.x * v2.x + v2.y * v2.y);
+		double c = (v1.x * v2.x + v1.y * v2.y) / (len1 * len2);
+		double r = len2 / len1;
+		cout << setw(2) << j << ") " << setw(7) << points[j].x << " " << setw(7) << points[j].y 
+			<< "\tv:" << setw(7) << v2.x << setw(7) << v2.y << "\tlen:" << setw(9) << len2 
+			<< " ratio " << setw(9) << r 
+			<< "\tc: " << setw(9) << c 
+		<< endl;
 	}
+	
 	return 0;
 }
