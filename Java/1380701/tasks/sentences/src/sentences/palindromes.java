39305de64d29cdf9de12ff6ca11db7333880c41f
Виктор Краснов <vickr@inbox.ru>
2020-12-13 12:49:16 +0300

    + 1380701 Java (5 tasks): 3. Палиндромы

---
+++ Java/1380701/tasks/sentences/src/sentences/palindromes.java
@@ -0,0 +1,72 @@
+/* 3. Дано два файла с предложениями.  
+ * Дописать в конец второго файла предложения из первого файла, в которых есть симметричные слова.
+ */
+
+package sentences;
+
+import java.io.*;
+
+public class palindromes {
+
+	public static void main(String[] args) {
+		String fileName = "input.txt";
+		File file = new File(fileName);
+		if(!file.exists()) {
+	        System.out.println("Файл не найден: " + fileName);
+	        return;
+		}
+		if(0 == file.length()) {
+	        System.out.println("Файл пуст: " + fileName);
+	        return;
+		}
+		char[] buf = new char[(int) file.length()];
+		try {
+			FileReader reader = new FileReader("input.txt");
+			
+			int result = reader.read(buf);
+			if(result != -1) {
+				FileWriter writer = new FileWriter("output.txt", true);
+			
+				String str = new String(buf);
+				String[] sentences = str.split("[.]+");
+				boolean first = true;
+				for(String sentence : sentences) {
+	        		boolean hasPalindrome = false;
+		        	String[] words = sentence.split("[ \t,-;:\n]+");
+		        	for(String word : words) {
+		        		if(word.length() > 1) {
+			        		boolean isPalindrome = true;
+			        		int j = word.length() - 1;
+			        		for(int i = 0; i < word.length() / 2; i++, j--) {
+			        			if(Character.toLowerCase(word.charAt(i)) != Character.toLowerCase(word.charAt(j))) {
+			        				isPalindrome = false;
+			        				break;
+			        			}
+			        		}
+			        		if(isPalindrome) {
+			        			System.out.println("\"" + word + "\"");
+			        			hasPalindrome = true;
+			        			break;
+			        		}
+		        		}
+		        	} // for(word)
+		        	if(hasPalindrome) {
+		        		if(first) {
+		        			// отделяем содержимое файла от первого предложения
+		        			first = false;
+							writer.append('\n');
+		        		}
+						writer.write(sentence);
+						writer.append('.');
+		        	}
+				} // for(sentence)
+				writer.close();
+			}
+			
+			reader.close();
+	    } catch(IOException ex) {
+	        System.out.println(ex.getMessage());
+	    }       
+	}
+
+}
